29e489a518e180ee3985529e3eb21db60c235026
EUFrosty <bojanvelickovic98@gmail.com>
2024-10-26 18:57:00 +0200

    Pocetak razvoja CatalogAPI

---
+++ Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Api.Data;
+using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data;
+
+public class CatalogContext : ICatalogContext
+{
+    public CatalogContext()
+    {
+        var client = new MongoClient("mongodb://localhost:27017");
+        var database = client.GetDatabase("CatalogDB");
+
+        Products = database.GetCollection<Product>("Products");
+        CatalogContextSeed.SeedData(Products);
+    }
+    
+    public IMongoCollection<Product> Products { get; }
+    
+    
+}
