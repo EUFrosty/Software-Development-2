07342ac678e4f25fa422f2f6d41504af8a6a2465
EUFrosty <bojanvelickovic98@gmail.com>
2024-10-28 12:53:46 +0100

    Dodate ostale operacije za Product

--- Catalog.Api/Repositories/IProductRepository.cs
+++ Catalog.Api/Repositories/IProductRepository.cs
@@ -6,5 +6,11 @@ namespace Catalog.Api.Repositories;
 public interface IProductRepository
 {
     Task<IEnumerable<Product>> GetProducts();
-
+    
+    Task<Product> GetProductById(string id);
+    Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+    Task CreateProduct(Product product);
+    Task<bool> UpdateProduct(Product product);
+    Task<bool> DeleteProduct(string id);
+    
 }
--- Catalog.Api/Repositories/ProductRepository.cs
+++ Catalog.Api/Repositories/ProductRepository.cs
@@ -18,6 +18,31 @@ public class ProductRepository : IProductRepository
     {
         return await _context.Products.Find(p=>true).ToListAsync();
     }
-    
-    
+
+    public async Task<Product> GetProductById(string id)
+    {
+        return await _context.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+    {
+        return await _context.Products.Find(p => p.Category == categoryName).ToListAsync();
+    }
+
+    public async Task CreateProduct(Product product)
+    {
+        await _context.Products.InsertOneAsync(product);
+    }
+
+    public async Task<bool> UpdateProduct(Product product)
+    {
+        var updateResult = _context.Products.ReplaceOneAsync(p => p.Id == product.Id, product);
+        return updateResult.IsCompleted && updateResult.Result.ModifiedCount > 0;
+    }
+
+    public async Task<bool> DeleteProduct(string id)
+    {
+        var deleteResult = await _context.Products.DeleteOneAsync(p => p.Id == id);
+        return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+    }
 }
